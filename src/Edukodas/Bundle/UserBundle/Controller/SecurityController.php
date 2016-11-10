3e2aad1d5b693ade351f3c216e5964e5bba2e97b
Lukas <lukasceplikas@gmail.com>
2016-11-10 19:56:32 +0200

    Redirect to dashboard if logged in

--- src/Edukodas/Bundle/UserBundle/Controller/SecurityController.php
+++ src/Edukodas/Bundle/UserBundle/Controller/SecurityController.php
@@ -8,6 +8,7 @@ class SecurityController extends BaseController {
     protected function renderLogin(array $data)
     {
         $securityContext = $this->container->get('security.authorization_checker');
+
         if ($securityContext->isGranted('IS_AUTHENTICATED_FULLY')) {
             return $this->redirectToRoute('edukodas_user_homepage');
         }

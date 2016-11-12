9e81c3f4ae17ade03f91122cb591658b7237dc72
Domantas Petrauskas <domantas.pet@gmail.com>
2016-11-12 20:03:40 +0200

    Generate template bundle

---
+++ src/Edukodas/Bundle/TemplateBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Edukodas\Bundle\TemplateBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('EdukodasTemplateBundle:Default:index.html.twig');
+    }
+}
---
+++ src/Edukodas/Bundle/TemplateBundle/EdukodasTemplateBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Edukodas\Bundle\TemplateBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EdukodasTemplateBundle extends Bundle
+{
+}

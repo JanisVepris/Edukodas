bfaa947e8d898f5f24c2fc7628c00d7b853388ab
GENADIJ BOJEV <genadij.bojev@gmail.com>
2016-11-14 00:01:59 +0200

    Add list and remove actions to controller

--- src/Edukodas/Bundle/TasksBundle/Controller/TasksController.php
+++ src/Edukodas/Bundle/TasksBundle/Controller/TasksController.php
@@ -12,16 +12,33 @@ class TasksController extends Controller
      */
     public function listAction()
     {
-//        $em = $this
-//            ->getDoctrine()
-//            ->getManager();
-//        $tasks = $em
-//            ->getRepository('EdukodasTasksBundle:Task')
-//            ->findAll();
-//
-//        return $this->render('EdukodasTasksBundle::tasks.html.twig', [
-//            'tasks' => $tasks,
-//        ]);
-        return $this->render('EdukodasTasksBundle::tasks.html.twig', []);
+        $em = $this
+            ->getDoctrine()
+            ->getManager();
+        $tasks = $em
+            ->getRepository('EdukodasTasksBundle:Task')
+            ->findAll();
+
+        return $this->render('EdukodasTasksBundle::tasks.html.twig', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    public function addAction()
+    {
+
+    }
+
+    public function removeAction($taskid)
+    {
+        $em = $this
+            ->getDoctrine()
+            ->getManager();
+        $task = $em
+            ->getRepository('EdukodasTasksBundle:Task')
+            ->findOneById($taskid);
+        $em->remove($task);
+        $em->flush();
+        return $this->redirectToRoute('edukodas_tasks_list');
     }
 }

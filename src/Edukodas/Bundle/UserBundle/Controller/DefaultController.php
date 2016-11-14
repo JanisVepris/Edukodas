f4e13da6f0f08321d5d624ad13d0852590b61085
GENADIJ BOJEV <genadij.bojev@gmail.com>
2016-11-14 22:07:39 +0200

    Add docblock

--- src/Edukodas/Bundle/UserBundle/Controller/DefaultController.php
+++ src/Edukodas/Bundle/UserBundle/Controller/DefaultController.php
@@ -7,8 +7,13 @@ use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
+    /**
+     * Returns an empty response as a placeholder for dashboard
+     *
+     * @return Response
+     */
     public function indexAction()
     {
-        return new Response('it\'s working');
+        return new Response('');
     }
 }

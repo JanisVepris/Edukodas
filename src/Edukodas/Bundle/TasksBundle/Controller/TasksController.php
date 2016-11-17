ca331d0d8d29692572ffa624d20e98d7031f585d
GENADIJ BOJEV <genadij.bojev@gmail.com>
2016-11-17 16:29:05 +0200

    Return json document of tasks

--- src/Edukodas/Bundle/TasksBundle/Controller/TasksController.php
+++ src/Edukodas/Bundle/TasksBundle/Controller/TasksController.php
@@ -2,63 +2,30 @@
 
 namespace Edukodas\Bundle\TasksBundle\Controller;
 
+use Edukodas\Bundle\TasksBundle\Entity\Course;
+use Edukodas\Bundle\TasksBundle\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class TasksController extends Controller
 {
     /**
-     * @return Response
+     * @return JsonResponse
      */
     public function listAction()
     {
-        $user = $this
-            ->getUser()
-            ->getId();
+        $courses = $this->getUser()->getCourses();
 
-        $em = $this
-            ->getDoctrine()
-            ->getManager();
-
-        $courses = $em
-            ->getRepository('EdukodasTasksBundle:Course')
-            ->findBy(['user' => $user]);
-
-        $tasks = $em
-            ->getRepository('EdukodasTasksBundle:Task')
-            ->findBy(['course' => $courses]);
-        $result = [];
-
-        foreach ($tasks as $task) {
-            $result[] = [
-                'courseId' => $task->getCourse()->getId(),
-                'courseName' => $task->getCourse()->getName(),
-                'taskId' => $task->getId(),
-                'taskName' => $task->getName(),
-                'taskDescription' => $task->getDescription(),
-                'taskPoints' => $task->getPoints(),
+        return new JsonResponse(array_map(function (Course $course) {
+            return [
+                'id' => $course->getId(),
+                'tasks' => array_map(function (Task $task) {
+                    return [
+                        'id' => $task->getId(),
+                        'name' => $task->getName(),
+                    ];
+                }, $course->getTasks()->toArray())
             ];
-        }
-
-        return new JsonResponse($result);
-    }
-
-    public function addAction()
-    {
-
-    }
-
-    public function removeAction($taskid)
-    {
-        $em = $this
-            ->getDoctrine()
-            ->getManager();
-        $task = $em
-            ->getRepository('EdukodasTasksBundle:Task')
-            ->findOneById($taskid);
-        $em->remove($task);
-        $em->flush();
-        return $this->redirectToRoute('edukodas_tasks_list');
+        }, $courses->toArray()));
     }
 }

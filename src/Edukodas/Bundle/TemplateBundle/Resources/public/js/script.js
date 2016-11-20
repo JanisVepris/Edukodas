55f9b5d4cdbd124d7186ed39917fea39c403e633
GENADIJ BOJEV <genadij.bojev@gmail.com>
2016-11-20 16:03:04 +0200

    Add FOSJsRoutingBundle

--- src/Edukodas/Bundle/TemplateBundle/Resources/public/js/script.js
+++ src/Edukodas/Bundle/TemplateBundle/Resources/public/js/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-    $('.modal').modal();
+    function editTaskButtonPressed(){
+        var url = Routing.generate('edukodas_tasks_edit', {taskId : 1});
+        // $.post(url, null, function (data) {
+        $('#edit-task-modal > .modal-content').html(url);
+        // });
+    }
+
+    $('#edit-task-modal').modal({
+            ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+                editTaskButtonPressed();
+            },
+            complete: function() { } // Callback for Modal close
+        }
+    );
 });

c8b89da757d4c524a9bd42fd8f18280f85d42ed8
GENADIJ BOJEV <genadij.bojev@gmail.com>
2016-11-24 19:43:20 +0200

    WIP: Point history. Add tasks error handling and security

---
+++ src/Edukodas/Bundle/StatisticsBundle/Controller/PointHistoryController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Edukodas\Bundle\StatisticsBundle\Controller;
+
+use Edukodas\Bundle\StatisticsBundle\Entity\PointHistory;
+use Edukodas\Bundle\StatisticsBundle\Form\PointHistoryType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class PointHistoryController extends Controller
+{
+    public function listAction()
+    {
+        $user = $this->getUser();
+
+        return $this->render('EdukodasTemplateBundle:pointhistory:listpoints.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+}

ccd4e8c9b8ca0bfffde0fb1d6b4964fd3990cf30
0lukas <lukas.ceplikas@gmail.com>
2016-11-27 22:33:38 +0200

    Use PointHistoryRepository

--- src/Edukodas/Bundle/ProfileBundle/Controller/StudentProfileController.php
+++ src/Edukodas/Bundle/ProfileBundle/Controller/StudentProfileController.php
@@ -5,6 +5,7 @@ namespace Edukodas\Bundle\ProfileBundle\Controller;
 use Edukodas\Bundle\StatisticsBundle\Entity\PointHistory;
 use Edukodas\Bundle\StatisticsBundle\Form\PointHistoryType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class StudentProfileController extends Controller
@@ -21,24 +22,19 @@ class StudentProfileController extends Controller
             throw new NotFoundHttpException('User not found');
         }
 
+        if (!in_array('ROLE_STUDENT', $user->getRoles())) {
+            throw new HttpException(400, 'User is not student');
+        }
+
         $pointHistory = $this
             ->getDoctrine()
             ->getRepository('EdukodasStatisticsBundle:PointHistory')
-            ->findBy(['student' => $user]);
-
-        $teacherPointHistory = $this
-            ->getDoctrine()
-            ->getRepository('EdukodasStatisticsBundle:PointHistory')
-            ->findBy(['student' => $user, 'teacher' => $this->getUser()]);
+            ->getRecentEntriesByStudent($user);
 
         $studentPoints = $this
             ->getDoctrine()
             ->getRepository('EdukodasStatisticsBundle:PointHistory')
-            ->createQueryBuilder('sp')
-            ->select('SUM(sp.amount)')
-            ->where('sp.student = ' . $user->getId())
-            ->getQuery()
-            ->getSingleScalarResult();
+            ->getTotalPointsByStudent($user);
 
         $points = new PointHistory();
         $pointsForm = $this->createForm(PointHistoryType::class, $points, ['user' => $user]);
@@ -48,7 +44,6 @@ class StudentProfileController extends Controller
             'teacher' => $this->getUser(),
             'points' => $studentPoints,
             'pointHistory' => $pointHistory,
-            'teacherPointHistory' => $teacherPointHistory,
             'addPointsForm' => $pointsForm->createView()
         ]);
     }

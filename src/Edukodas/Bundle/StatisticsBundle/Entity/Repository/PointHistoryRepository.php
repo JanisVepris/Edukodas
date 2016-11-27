0a49e8e9e4b86af8deddd2094b5bb31b201af31a
0lukas <lukas.ceplikas@gmail.com>
2016-11-27 22:32:59 +0200

    Add recent entries and total points by student

--- src/Edukodas/Bundle/StatisticsBundle/Entity/Repository/PointHistoryRepository.php
+++ src/Edukodas/Bundle/StatisticsBundle/Entity/Repository/PointHistoryRepository.php
@@ -2,6 +2,7 @@
 
 namespace Edukodas\Bundle\StatisticsBundle\Entity\Repository;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\EntityRepository;
 use Edukodas\Bundle\StatisticsBundle\Entity\PointHistory;
@@ -29,6 +30,56 @@ class PointHistoryRepository extends EntityRepository
             ->getResult();
 
         $this->getEntityManager()->getFilters()->enable('softdeleteable');
+
+        return $result;
+    }
+
+    /**
+     * @param User $student
+     * @param int $maxEntries
+     *
+     * @return Collection|PointHistory[]
+     */
+    public function getRecentEntriesByStudent(User $student, $maxEntries = 10): Array
+    {
+        $this->getEntityManager()->getFilters()->disable('softdeleteable');
+
+        $result = $this
+            ->createQueryBuilder('ph')
+            ->where('ph.student = :student')
+            ->orderBy('ph.id', 'DESC')
+            ->setMaxResults($maxEntries)
+            ->setParameter('student', $student)
+            ->getQuery()
+            ->getResult();
+
+        $this->getEntityManager()->getFilters()->enable('softdeleteable');
+
+        return $result;
+    }
+
+    /**
+     * @param User $student
+     *
+     * @return int
+     */
+    public function getTotalPointsByStudent(User $student): int
+    {
+        $this->getEntityManager()->getFilters()->disable('softdeleteable');
+
+        $result = $this
+            ->createQueryBuilder('ph')
+            ->select('SUM(ph.amount)')
+            ->where('ph.student = :student')
+            ->setParameter('student', $student)
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        $this->getEntityManager()->getFilters()->enable('softdeleteable');
+
+        if (!$result) {
+            return 0;
+        }
 
         return $result;
     }

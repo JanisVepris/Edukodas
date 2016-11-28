6606dd894e47c74aa714d275a2708c96c5bb482d
0lukas <lukas.ceplikas@gmail.com>
2016-11-28 20:07:32 +0200

    FIX: Code style

--- src/Edukodas/Bundle/FixturesBundle/DataFixtures/ORM/LoadStudentGenerationData.php
+++ src/Edukodas/Bundle/FixturesBundle/DataFixtures/ORM/LoadStudentGenerationData.php
@@ -2,7 +2,6 @@
 
 namespace UserBundle\DataFixture\ORM;
 
-
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;

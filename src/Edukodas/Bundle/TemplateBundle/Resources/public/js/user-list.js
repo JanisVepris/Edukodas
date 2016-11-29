121179c6445acb05a656a55e9ab60a1d361dfe17
0lukas <lukas.ceplikas@gmail.com>
2016-11-29 19:28:38 +0200

    Student list by team, class

--- src/Edukodas/Bundle/TemplateBundle/Resources/public/js/user-list.js
+++ src/Edukodas/Bundle/TemplateBundle/Resources/public/js/user-list.js
@@ -3,31 +3,32 @@ $(document).ready(function() {
 
     var selectTeam = $("#select-team");
     var selectClass = $("#select-class");
+    var userList = $("#user-list");
 
     var updateUserList = function() {
+        var url = Routing.generate('edukodas_user_list_update');
+
         $.ajax({
-            url: Routing.generate('edukodas_user_list_update'),
-            data: {
-                team: selectTeam.val(),
-                class: selectClass.val()
-            },
+            url: url,
             type: 'POST',
+            data: {
+                teamId: selectTeam.val(),
+                classId: selectClass.val()
+            },
             beforeSend: function() {
-                // $('.delete-task*[data-task-id="' + taskId + '"]').prop('disabled',true).hide();
-                // $('#delete-task-preload-' + taskId).removeClass('hide');
+                //
             },
             success: function(data) {
                 if (data) {
-                    console.log(data);
+                    userList.html(data);
                 }
             },
             error: function() {
                 Materialize.toast('Nepavyko atnaujinti sąrašo', 4000);
-                // $('#delete-task-preload-' + taskId).addClass('hide');
-                // $('.delete-task*[data-task-id="' + taskId + '"]').prop('disabled',false).show();
             }
         });
-    }
+    };
 
     selectTeam.on("change", updateUserList);
+    selectClass.on("change", updateUserList);
 });
--- src/Edukodas/Bundle/UserBundle/Controller/UserListController.php
+++ src/Edukodas/Bundle/UserBundle/Controller/UserListController.php
@@ -2,6 +2,7 @@
 
 namespace Edukodas\Bundle\UserBundle\Controller;
 
+use Edukodas\Bundle\UserBundle\Entity\StudentTeam;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -9,24 +10,54 @@ class UserListController extends Controller
 {
     public function indexAction()
     {
+        $teamList = $this->getDoctrine()
+            ->getRepository('EdukodasUserBundle:StudentTeam')
+            ->findAll();
+
+        $classList = $this->getDoctrine()
+            ->getRepository('EdukodasUserBundle:StudentClass')
+            ->findAll();
+
+        $statisticsService = $this->get('edukodas.statistics');
+
+        $userList = $statisticsService->getStudentList();
+        $maxAmount = $userList->first()['amount'];
+
         return $this->render('@EdukodasTemplate/Users/list.html.twig', [
-//            'teamList' => $teamList,
-//            'classList' => $classList,
+            'teamList' => $teamList,
+            'classList' => $classList,
+            'userList' => $userList,
+            'maxAmount' => $maxAmount,
         ]);
     }
 
     public function listAction(Request $request)
     {
-        $team = $request->request->get('team');
-        $class = $request->request->get('class');
+        $team = $this->getDoctrine()
+            ->getRepository('EdukodasUserBundle:StudentTeam')
+            ->find($request->request->get('teamId'));
 
-        $users = $this
-            ->getDoctrine()
-            ->getRepository('EdukodasStatisticsBundle:PointHistory')
-            ->getStudentPointTotals();
+        $class = $this->getDoctrine()
+            ->getRepository('EdukodasUserBundle:StudentClass')
+            ->find($request->request->get('classId'));
+
+        $statisticsService = $this->get('edukodas.statistics');
+
+        if ($team && $class) {
+            $userList = $statisticsService->getStudentListByTeamAndClass($team, $class);
+        } elseif ($team) {
+            $userList = $statisticsService->getStudentListByTeam($team);
+        } elseif ($class) {
+            $userList = $statisticsService->getStudentListByClass($class);
+        } else {
+            $userList = $statisticsService->getStudentList();
+        }
+
+        $maxAmount = $userList->first()['amount'];
 
         return $this->render('@EdukodasTemplate/Users/inc/_listUsers.html.twig', [
-            'users' => $users,
+            'userList' => $userList,
+            'maxAmount' => $maxAmount,
         ]);
     }
 }

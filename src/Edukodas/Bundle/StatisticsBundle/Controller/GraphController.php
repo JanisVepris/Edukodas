c09303faa9d13bba2bf0f917b2ea2375a90cad02
0lukas <lukas.ceplikas@gmail.com>
2016-12-09 17:30:07 +0200

    Add team cumulative points graph

--- src/Edukodas/Bundle/StatisticsBundle/Controller/GraphController.php
+++ src/Edukodas/Bundle/StatisticsBundle/Controller/GraphController.php
@@ -24,11 +24,15 @@ class GraphController extends Controller
         $teamPieAndBarGraph = $graphService->getTeamPieAndBarChartGraph($timespan, $team, $class);
         $teamLineGraph = $graphService->getTeamLineChartGraph($timespan, $team, $class);
         $topUsersGraph = $graphService->getTopUsersBarChartGraph(static::TOP_USER_LIMIT, $timespan, $team, $class);
+        $teamLineCumulativeGraph = $graphService->getTeamLineCumulativeChartGraph($timespan, $team, $class);
+
+        //dump($a);die;
 
         return $this->render('@EdukodasTemplate/Graph/graph.html.twig', [
             'filterForm' => $filterForm->createView(),
             'teamPieAndBarGraph' => $teamPieAndBarGraph,
             'teamLineGraph' => $teamLineGraph,
+            'teamLineCumulativeGraph' => $teamLineCumulativeGraph,
             'topUsersGraph' => $topUsersGraph,
         ]);
     }

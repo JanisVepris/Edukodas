3011970a77fd2bbe091c4901b35ad952b7182504
Domantas Petrauskas <domantas.pet@gmail.com>
2016-12-10 13:02:09 +0200

    Selectize graph page dropdowns

--- src/Edukodas/Bundle/TemplateBundle/Resources/public/js/graph.js
+++ src/Edukodas/Bundle/TemplateBundle/Resources/public/js/graph.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-    $('[name=graph] select').material_select();
+    $('#graph_timespan').material_select();
+    $('#graph_team').selectize();
+    $('#graph_class').selectize();
 
     $('[name=graph] select').on('change', function () {
         $('[name=graph]').submit();
